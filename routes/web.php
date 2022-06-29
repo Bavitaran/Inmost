e32ad9546b6c5782ae79d1b9a82eb4e58a29bf63
Bavitaran <bavitaran95@gmail.com>
2022-06-29 10:26:33 +0800

    Add template.

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,9 @@ Route::get('/', function () {
     return redirect(route('login'));
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/admindashboard', function () { return view('dashboard');})->name('dashboard');
+    Route::get('/dashboard', function () { return view('userdashboard');});
+    
 });
